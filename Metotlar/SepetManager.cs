035370c7abe81e5c92110958cf7c0a4008ce8b1d
Gorke <Gorke@GRKEM>
2024-06-29 16:57:43 +0300

    Add project files.

---
+++ Metotlar/SepetManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Metotlar
+{
+    internal class SepetManager
+    {
+        //Naming Convention
+        //Syntax
+        public void Ekle(Urun urun) 
+        {
+            Console.WriteLine(" Tebrikler. Sepete Eklendi : " + urun.Adi);
+            
+        }
+
+        public void Ekle2(string urunadi, string aaciklama, double fiyat, int stokAdedi)
+        {
+            Console.WriteLine("Tebrikler. Sepete Eklendi : " + urunadi);
+        }
+    }
+}
